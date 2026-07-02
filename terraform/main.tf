9be4a3f0f2422fdf32e6c2698b564c49583afc47
Jayesh <jayeshzimbal@gmail.com>
2026-07-02 13:45:43 +0530

    update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -70,7 +70,16 @@ module "eks" {
 
   cluster_endpoint_public_access = true
 
+    vpc_id = module.vpc.vpc_id
+
   subnet_ids = module.vpc.private_subnets
+
+  control_plane_subnet_ids = module.vpc.private_subnets
+
+  depends_on = [
+    module.vpc
+  ]
+
 
   enable_cluster_creator_admin_permissions = true
 
