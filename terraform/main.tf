797f3aab7fdb641e1c6743378a8bc853a965ed1f
Jayesh <jayeshzimbal@gmail.com>
2026-07-02 16:18:05 +0530

    update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -95,9 +95,9 @@ module "eks" {
 
   eks_managed_node_groups = {
     workers = {
-      min_size     = 1
-      max_size     = 3
-      desired_size = 2
+      min_size     = 2
+      max_size     = 2
+      desired_size = 3
     }
   }
 
